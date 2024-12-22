4f24125d098e12e738fbd0cca0d3d70fe9e35207
luvnft <140175751+luvnft@users.noreply.github.com>
2024-12-22 15:33:41 -0500

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,23 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Adjust chunk size warning limit
+  build: {
+    chunkSizeWarningLimit: 2000, // Set the chunk size warning limit to 2MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Create a separate vendor chunk for node_modules dependencies
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
+  
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // Exclude lucide-react from optimization
   },
 });
